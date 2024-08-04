8e4a396530bba67c84554b8fb2799eef1d6c89f7
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2024-08-04 23:01:25 +0100

    feat(webAndSecurityConfigs): add basic configs for web and security

---
+++ src/main/java/org/plumdev/serve/config/MvcConfig.java
@@ -0,0 +1,14 @@
+package org.plumdev.serve.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/media").setViewName("media");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
