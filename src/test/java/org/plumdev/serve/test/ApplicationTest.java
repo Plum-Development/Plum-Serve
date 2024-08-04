cf8458f2bb8eb9dba3e0391734ef99664a96c869
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2024-08-04 23:02:50 +0100

    test(ApplicationAndController): add basic tests (still broken) for some new classes

---
+++ src/test/java/org/plumdev/serve/test/ApplicationTest.java
@@ -0,0 +1,19 @@
+package org.plumdev.serve.test;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class ApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
---
+++ src/test/java/org/plumdev/serve/test/controllers/AbstractControllerTest.java
@@ -0,0 +1,30 @@
+package org.plumdev.serve.test.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.plumdev.serve.config.MvcConfig;
+import org.plumdev.serve.config.WebSecurityConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@Import({
+        MvcConfig.class,
+        WebSecurityConfiguration.class
+})
+public abstract class AbstractControllerTest {
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected WebApplicationContext context;
+
+    @BeforeEach
+    public void abstractSetUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+    }
+}
