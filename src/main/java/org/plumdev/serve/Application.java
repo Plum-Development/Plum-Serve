bd1ab5e23032844119c69cfa346270298e1e883e
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2024-08-04 17:01:50 +0100

    chore(init): initial setup, spring skeleton

---
+++ src/main/java/org/plumdev/serve/Application.java
@@ -0,0 +1,15 @@
+package org.plumdev.serve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(Application.class, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/plumdev/serve/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package org.plumdev.serve.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+    @GetMapping
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
