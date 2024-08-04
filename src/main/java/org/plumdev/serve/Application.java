4a8b88a8dfee94ad84de96fabf17a7e7f0afda0d
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2024-08-04 17:56:07 +0100

    chore(Application): remove try catch

--- src/main/java/org/plumdev/serve/Application.java
+++ src/main/java/org/plumdev/serve/Application.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        try {
-            SpringApplication.run(Application.class, args);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        SpringApplication.run(Application.class, args);
     }
 }
